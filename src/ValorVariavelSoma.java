e32caa6718083fdd804bb9b1cbba038debc16431
Juan Martins Caitano <juanmartinscaitano@gmail.com>
2024-08-31 16:11:35 -0300

    feature: adicionado os exercicios da vaga

---
+++ src/ValorVariavelSoma.java
@@ -0,0 +1,9 @@
+public class ValorVariavelSoma {
+    public static void main(String[] args) {
+
+
+
+//Observe o trecho de código abaixo: int INDICE = 12, SOMA = 0, K = 1; enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; } imprimir(SOMA);
+//Ao final do processamento, qual será o valor da variável SOMA?
+    }
+}
