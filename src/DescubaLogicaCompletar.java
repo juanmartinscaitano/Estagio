b0731e1a9caab209bce8054abc48e8b5817a7074
Juan Martins Caitano <juanmartinscaitano@gmail.com>
2024-09-01 13:47:29 -0300

    feature: adicionado um println para mostrar o raciocinio e o resultado das questoes a-f

--- src/DescubaLogicaCompletar.java
+++ src/DescubaLogicaCompletar.java
@@ -1,13 +1,28 @@
 public class DescubaLogicaCompletar {
     public static void main(String[] args) {
+        System.out.println("Logica da questão a) é adicionar 2 ao próximo número, sendo assim o valor do próximo elemento seria: 9");
+        //a)
+        System.out.println("Logica da questão b) é  multiplicar por 2 o próximo número, sendo assim o valor do próximo elemento seria: 128");
+        //b)
+        System.out.println("Logica da questão c) é somar o próximo numero ultilizando apenas números primos, sendo assim o valor do próximo elemento seria: 49");
+        //c)
+        System.out.println("Logica da questão d) é adicionar no primeiro numero 12(os proximos irao adicionar 8 em cada repeticao" +
+                " sendo ex: 12, 12+8, 20+8,28+8) sendo a resposta o número: 100");
+        //d)
+        System.out.println("Logica da questão e) é ultilizar o método fibonacci, sendo assim o valor do próximo elemento seria: 13");
+        //e)
+        System.out.println("Logica da questão f) é utilizar a seuquência '+10, +2, +4, +1, +1, +1' (os próximos números seriam a soma do anterior + 1), sendo assim o valor do próximo elemento seria: 20");
+        //f)
+    }
+}
 
 
-
+//Descubra a lógica e complete o próximo elemento:
 //a) 1, 3, 5, 7, ___
 //b) 2, 4, 8, 16, 32, 64, ____
 //c) 0, 1, 4, 9, 16, 25, 36, ____
 //d) 4, 16, 36, 64, ____
 //e) 1, 1, 2, 3, 5, 8, ____
 //f) 2,10, 12, 16, 17, 18, 19, ____
-    }
-}
+
+//resultado em ordem: 9, 128, 49, 100, 13 e 20.
