7bca812a4fdc989f0dc256df7249d724a7e79dfb
Juan Martins Caitano <juanmartinscaitano@gmail.com>
2024-09-01 11:34:44 -0300

    feature: adicionado o metodo para dizer se tem a letra 'a', sendo maiuscula ou minuscula

--- src/ExisteA.java
+++ src/ExisteA.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 public class ExisteA {
     public static void main(String[] args) {
+        String contem = "Sim, tem a letra 'a' na palvra/frase! ";
+        String naoContem = "Não tem a letra 'a' na palvra/frase! ";
+
+        System.out.println("Digite uma palavra/frase: ");
+        String existeA = new Scanner(System.in).nextLine();
+        if (existeA.contains("a") || existeA.contains("A")) {
+            System.out.println(contem);
+        } else
+            System.out.println(naoContem);
 
 
+    }
+}
 
 // Escreva um programa que verifique, em uma string, a existência da letra ‘a’, seja maiúscula ou minúscula, além de informar
 // a quantidade de vezes em que ela ocorre.
 //IMPORTANTE: Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código;
-    }
-}
