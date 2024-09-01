3d7d7d44cb87810b2d6521484e2737242da6cf78
Juan Martins Caitano <juanmartinscaitano@gmail.com>
2024-09-01 18:01:39 -0300

    feature: adicionado um println com a resolucao do problema que encontrei

--- src/Interruptor.java
+++ src/Interruptor.java
@@ -2,10 +2,12 @@ public class Interruptor {
     public static void main(String[] args) {
 
 
+        System.out.println("Usaria uma estratégia de deixar  um ligado, ai sairia da sala A para a B e : deixaria o interruptores I  ligado e iria da sala A para a B, se estivesse ligado saberiamos que I = B, depois ligassemos o interruptor II " +
+                "iria até a sala C e se estivesse ligada II = C && III = a, se não estivesse ligada II = a && III = c");
+    }
+}
 
 // Você está em uma sala com três interruptores, cada um conectado a uma lâmpada em salas diferentes. Você não pode ver
 // as lâmpadas da sala em que está, mas pode ligar e desligar os interruptores quantas vezes quiser. Seu objetivo é descobrir
 // qual interruptor controla qual lâmpada. Como você faria para descobrir, usando apenas duas idas até uma das salas das lâmpadas,
 // qual interruptor controla cada lâmpada?
-    }
-}
