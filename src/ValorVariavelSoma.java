21eba2dddb488f3741a0f05bc13d5dbccbf75c96
Juan Martins Caitano <juanmartinscaitano@gmail.com>
2024-09-01 13:07:04 -0300

    feature: adicionado um metodo que imprime o valor da variavel 'SOMA' enquanto a condicao do while for verdadeira

--- src/ValorVariavelSoma.java
+++ src/ValorVariavelSoma.java
@@ -1,9 +1,16 @@
 public class ValorVariavelSoma {
     public static void main(String[] args) {
+    int INDICE = 12;
+    int SOMA = 0, K = 1;
 
-
+    while (K < INDICE) {
+        K = K + 1;
+        SOMA = SOMA + K;
+    }
+        System.out.println(SOMA);
+    }
+}
 
 //Observe o trecho de código abaixo: int INDICE = 12, SOMA = 0, K = 1; enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; } imprimir(SOMA);
 //Ao final do processamento, qual será o valor da variável SOMA?
-    }
-}
+// RESPOSTA: O valor da soma será 77
